cc54348b24210ee25ef933a1d79cdf2b6e479c30
DamonXue <damonxue2@gmail.com>
2024-09-20 23:11:45 +0800

    ci +10.

--- .github/workflows/ci.bat
+++ .github/workflows/ci.bat
@@ -13,13 +13,13 @@ rem If a test crashes, we want to know which one it was.
 set RUST_TEST_THREADS=1
 set RUST_BACKTRACE=1
 
-%~dp0%~nx0 test --target "%TARGET%"
-%~dp0%~nx0 test --target "%TARGET%" --release
+%CARGO% test --target "%TARGET%"
+%CARGO% test --target "%TARGET%" --release
 
-%~dp0%~nx0 test --target "%TARGET%" --all-features
-%~dp0%~nx0 test --target "%TARGET%" --all-features --release
+%CARGO% test --target "%TARGET%" --all-features
+%CARGO% test --target "%TARGET%" --all-features --release
 
 timeout /t 10
-%~dp0%~nx0 run --package examples --all-features --bin axum_example || exit /b 0
+%CARGO% run --package examples --all-features --bin axum_example || exit /b 0
 timeout /t 10
-%~dp0%~nx0 run --package examples --all-features --bin actix_web_example || exit /b 0
+%CARGO% run --package examples --all-features --bin actix_web_example || exit /b 0
