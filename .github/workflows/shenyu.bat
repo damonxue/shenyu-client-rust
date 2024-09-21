d957b95982e6f9fd25ba6d82b4dc0b69665950bf
DamonXue <damonxue2@gmail.com>
2024-09-21 10:30:15 +0800

    ci +11.

--- .github/workflows/shenyu.bat
+++ .github/workflows/shenyu.bat
@@ -1,19 +1,11 @@
 cd %USERPROFILE%
 git config --system core.longpaths true
 git clone https://github.com/apache/shenyu
-cd shenyu
-mvn clean -Prelease -DskipTests package -pl ./shenyu-dist/shenyu-admin-dist -am -U
-
-set "targetName=shenyu-admin.tar.gz"
-set sourcePath=%USERPROFILE%\shenyu\shenyu-dist\shenyu-admin-dist\target
-echo Source path: %sourcePath%
-
-for %%f in (%sourcePath%\apache-shenyu*.tar.gz) do (
-    ren "%%f" "%targetName%"
-    echo Renamed %%f to %targetName%
-)
-
-cd %sourcePath%
+cd shenyu/shenyu-dist/shenyu-admin-dist
+(Get-Content /shenyu-admin-dist.pom) -replace '<finalName>.*</finalName>', '<finalName>shenyu-admin</finalName>' | Set-Content /shenyu-admin-dist.pom
+cd %USERPROFILE%\shenyu
+mvn clean -Prelease -Dmaven.javadoc.skip=true -B -Drat.skip=true -Djacoco.skip=true -DskipITs -DskipTests package -pl ./shenyu-dist/shenyu-admin-dist -am -U
+cd shenyu-dist\shenyu-admin-dist\target
 tar -xzf shenyu-admin.tar.gz
 cd shenyu-admin\bin
 .\start.bat
