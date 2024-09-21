ca6c7019258c23719e4a88cbb05de344bfa65f43
DamonXue <damonxue2@gmail.com>
2024-09-21 13:00:18 +0800

    ci +12.

--- .github/workflows/shenyu.bat
+++ .github/workflows/shenyu.bat
@@ -1,6 +1,7 @@
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/libarchive/libarchive/releases/download/v3.7.5/libarchive-3.7.5.zip' -OutFile 'tar.zip'"
 powershell -Command "Expand-Archive -Path 'tar.zip' -DestinationPath '%GITHUB_WORKSPACE%\tar'"
 powershell -Command "Add-Content -Path ‘%GITHUB_PATH%’ -Value '%GITHUB_WORKSPACE%\tar\bin'"
+tar --version
 
 git config --system core.longpaths true
 git clone https://github.com/apache/shenyu
@@ -10,6 +11,6 @@ powershell -Command "(Get-Content %pomFile%) -replace '<finalName>.*</finalName>
 cd ../../
 mvn clean -Prelease -Dmaven.javadoc.skip=true -B -Drat.skip=true -Djacoco.skip=true -DskipITs -DskipTests package -pl ./shenyu-dist/shenyu-admin-dist -am -U
 cd shenyu-dist\shenyu-admin-dist\target
-powershell -Command "Expand-Archive -Path shenyu-admin-admin-bin.tar.gz -DestinationPath ."
+tar -zxvf shenyu-admin-admin-bin.tar.gz
 cd shenyu-admin-admin-bin\bin
 .\start.bat
