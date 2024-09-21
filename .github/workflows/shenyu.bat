054024ef45831cc74ca6a99894e82184459a661a
DamonXue <damonxue2@gmail.com>
2024-09-21 12:57:50 +0800

    ci +12.

--- .github/workflows/shenyu.bat
+++ .github/workflows/shenyu.bat
@@ -1,6 +1,6 @@
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/libarchive/libarchive/releases/download/v3.7.5/libarchive-3.7.5.zip' -OutFile 'tar.zip'"
 powershell -Command "Expand-Archive -Path 'tar.zip' -DestinationPath '%GITHUB_WORKSPACE%\tar'"
-powershell -Command "Add-Content -Path $env:GITHUB_PATH -Value '%GITHUB_WORKSPACE%\tar\bin'"
+powershell -Command "Add-Content -Path ‘%GITHUB_PATH%’ -Value '%GITHUB_WORKSPACE%\tar\bin'"
 
 git config --system core.longpaths true
 git clone https://github.com/apache/shenyu
