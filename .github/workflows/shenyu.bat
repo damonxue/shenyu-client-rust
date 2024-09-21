b05a9c9d0ef93fddbd3f72f8a9f563b49a7fb802
DamonXue <damonxue2@gmail.com>
2024-09-21 10:52:22 +0800

    ci +11.

--- .github/workflows/shenyu.bat
+++ .github/workflows/shenyu.bat
@@ -4,7 +4,7 @@ git clone https://github.com/apache/shenyu
 cd shenyu/shenyu-dist/shenyu-admin-dist
 set pomFile="pom.xml"
 powershell -Command "(Get-Content %pomFile%) -replace '<finalName>.*</finalName>', '<finalName>shenyu-admin</finalName>' | Set-Content %pomFile%"
-cd %USERPROFILE%\shenyu
+cd ../../
 mvn clean -Prelease -Dmaven.javadoc.skip=true -B -Drat.skip=true -Djacoco.skip=true -DskipITs -DskipTests package -pl ./shenyu-dist/shenyu-admin-dist -am -U
 cd shenyu-dist\shenyu-admin-dist\target
 tar -xzf shenyu-admin.tar.gz
