2d3e0249cb03e237a3d216be19ced9e6c7606952
DamonXue <damonxue2@gmail.com>
2024-09-21 10:39:06 +0800

    ci +11.

--- .github/workflows/shenyu.bat
+++ .github/workflows/shenyu.bat
@@ -2,7 +2,7 @@ cd %USERPROFILE%
 git config --system core.longpaths true
 git clone https://github.com/apache/shenyu
 cd shenyu/shenyu-dist/shenyu-admin-dist
-(Get-Content "pom.xml") -replace '<finalName>.*</finalName>', '<finalName>shenyu-admin</finalName>' | Set-Content "pom.xml"
+(Get-Content pom.xml) -replace '<finalName>.*</finalName>', '<finalName>shenyu-admin</finalName>' | Set-Content pom.xml
 cd %USERPROFILE%\shenyu
 mvn clean -Prelease -Dmaven.javadoc.skip=true -B -Drat.skip=true -Djacoco.skip=true -DskipITs -DskipTests package -pl ./shenyu-dist/shenyu-admin-dist -am -U
 cd shenyu-dist\shenyu-admin-dist\target
